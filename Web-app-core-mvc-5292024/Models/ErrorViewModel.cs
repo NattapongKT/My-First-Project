7b29b62bdb02e8de7d1ddceb3d2bfe4d0baa2cab
NattapongKT <NattapongK4257@gmail.com>
2024-05-30 09:43:07 +0700

    Add My First Project To Repository

---
+++ Web-app-core-mvc-5292024/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_app_core_mvc_5292024.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
